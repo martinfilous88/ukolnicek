fb75440ec680b9de5dcf21b6a00fcf3ba82872f7
Karel Kryštof Dostál <karel.krystof.dostal@beanstudent.cz>
2024-01-23 10:39:37 +0100

    Nová změna

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<App/>*/}
+    {<h1>Ahoj světe!</h1>}
+    <button>Kikni na mě</button>
   </React.StrictMode>
 );
